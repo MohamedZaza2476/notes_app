5701d38dca6ae1be5dcd336675097ea213d12a26
MohamedZaza2476 <mzaza2476@gmail.com>
2026-04-18 16:36:16 +0200

    create colors list view part 1

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/colors_list_view.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -41,6 +42,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
             maxLines: 5,
           ),
           const SizedBox(height: 64),
+          ColorsListView(),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomButton(
@@ -50,7 +52,9 @@ class _AddNoteFormState extends State<AddNoteForm> {
                     formKey.currentState!.save();
 
                     var currentDate = DateTime.now();
-                    var formattedCurrentDate = DateFormat('dd MMMM, yyyy').format(currentDate);
+                    var formattedCurrentDate = DateFormat(
+                      'dd MMMM, yyyy',
+                    ).format(currentDate);
 
                     var noteModel = NoteModel(
                       title: title!,
